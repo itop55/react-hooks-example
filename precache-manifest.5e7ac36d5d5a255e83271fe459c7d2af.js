99f9567955838670a8d1f8402241102ff05b756a
itop55 <itop55@gmail.com>
2020-07-23 13:31:51 +0300

    Updates

---
+++ precache-manifest.5e7ac36d5d5a255e83271fe459c7d2af.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8330fa3d34f4f06fb7c0",
+    "url": "/react-hooks-example/static/css/main.894590e5.chunk.css"
+  },
+  {
+    "revision": "8330fa3d34f4f06fb7c0",
+    "url": "/react-hooks-example/static/js/main.ea4451ad.chunk.js"
+  },
+  {
+    "revision": "cb621a60e7d48be77e39",
+    "url": "/react-hooks-example/static/js/runtime~main.44fa859d.js"
+  },
+  {
+    "revision": "9adb1278dac37b9f50e7",
+    "url": "/react-hooks-example/static/css/2.efae3417.chunk.css"
+  },
+  {
+    "revision": "9adb1278dac37b9f50e7",
+    "url": "/react-hooks-example/static/js/2.3022a969.chunk.js"
+  },
+  {
+    "revision": "6c38427c6022f989e7865a4005c0173e",
+    "url": "/react-hooks-example/index.html"
+  }
+];
